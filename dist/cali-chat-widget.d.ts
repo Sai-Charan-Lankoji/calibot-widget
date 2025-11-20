58e9f11da3e499a2aed15b339e0cb6a9dc413d97
sai-charan-lankoji <saicharan.lankoji@calibrage.in>
2025-11-20 16:27:29 +0530

    Add dist folder for CDN

---
+++ dist/cali-chat-widget.d.ts
@@ -0,0 +1,19 @@
+export declare const CaliChatWidgetAPI: {
+    init: typeof init;
+};
+
+declare function init(config: WidgetConfig & {
+    containerId?: string;
+}): void;
+
+declare interface WidgetConfig {
+    botId: string;
+    apiBaseUrl: string;
+    primaryColor?: string;
+    avatarSrc?: string;
+    botName?: string;
+    welcomeMessage?: string;
+    position?: 'bottom-right' | 'bottom-left';
+}
+
+export { }
